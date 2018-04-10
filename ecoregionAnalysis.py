547383067cfa7ce2399d7fca804141c64b716371
Jeff Thompson <jeffery.a.thompson@colorado.edu>
2018-04-10 10:32:39 -0600

    looked at Level 2, Intermontane Biogreion; clusters 7 clusters; wrote image out

--- ecoregionAnalysis.py
+++ ecoregionAnalysis.py
@@ -7,26 +7,81 @@ Created on Mon Apr  2 17:21:41 2018
 """
 import numpy as np
 import rasterio
+import fiona
+import itertools
 from matplotlib import path
 import geopandas as gpd
 from affine import Affine 
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon#, shape, mapping
+from shapely.ops import cascaded_union#, unary_union
 from matplotlib import pyplot as plt
 from tqdm import tqdm 
 from sklearn.cluster import MiniBatchKMeans
 
-iDir = '/Users/jeth6160/Desktop/permafrost/Alaska/EPA/'
-fIn = 'AK_export_albers.shp'
+
+#iDir = '/Users/jeth6160/Desktop/permafrost/Alaska/EPA/'
+#fIn = 'AK_export_albers.shp'
+iDir = '/Users/jeth6160/Desktop/permafrost/Alaska/USGS/'
+#fIn = 'akecoregions.shp'
+fIn = 'akecoregions_intmontane.shp'
+shpIn = gpd.read_file(iDir+fIn)
+
 
 iDir2 = '/Users/jeth6160/Desktop/permafrost/Alaska/AppEARS/allAK/output/'
 fIn2 = 'TC_TrendsAll_v3.tif'
 
-shpIn = gpd.read_file(iDir+fIn)
+
 
 # FID for northern most shapefils 6,11,14
-roi = shpIn.iloc[6]
+#roi = shpIn.iloc[6]
+# this is the new level 2 ecoregion
+roi = shpIn.iloc[0]
 
+"""
+from shapely.geometry import shape, mapping
+from shapely.ops import unary_union
+import fiona
+import itertools
+with fiona.open('cb_2013_us_county_20m.shp') as input:
+    # preserve the schema of the original shapefile, including the crs
+    meta = input.meta
+    with fiona.open('dissolve.shp', 'w', **meta) as output:
+        # groupby clusters consecutive elements of an iterable which have the same key so you must first sort the features by the 'STATEFP' field
+        e = sorted(input, key=lambda k: k['properties']['STATEFP'])
+        # group by the 'STATEFP' field 
+        for key, group in itertools.groupby(e, key=lambda x:x['properties']['STATEFP']):
+            properties, geom = zip(*[(feature['properties'],shape(feature['geometry'])) for feature in group])
+            # write the feature, computing the unary_union of the elements in the group with the properties of the first element in the group
+            output.write({'geometry': mapping(unary_union(geom)), 'properties': properties[0]})
+
+
+
+
+#want to try select on 
+roiTemp=shpIn.loc[shpIn.LEVEL_2=='Intermontane Boreal']
+roiGeo = roiTemp[['LEVEL_2','geometry']]
+
+
+polys = Polygon(roiGeo.iloc[0,1],roiGeo.iloc[1,1])
+
+
+roiGeoMerge = cascaded_union(roiGeo)
+
+roi = shpIn[['LEVEL_2','geometry']]
+
+roi = shpIn[['LEVEL_2','geometry']]
+roiDis = roiTemp.dissolve(by='LEVEL_2',as_index=False)
+continents = world.dissolve(by='continent')
+#roi=shpIn.loc[shpIn.LEVEL_2=='Intermontane Boreal']
+roiPoly = roi.loc[:,'geometry']
+roiMerged = gpd.GeoSeries(cascaded_union(roiPoly))
+roiMerged = gpd.GeoSeries(cascaded_union(roi.geom))
+
+#roi = cascaded_union(roi.loc[:,'geometry'])
+"""
 x,y = roi['geometry'].exterior.coords.xy
+#x,y = roiGeo.loc['geometry'].exterior.coords.xy
+
 x = np.array(x)
 y = np.array(y)
 
@@ -85,16 +140,17 @@ plt.ylabel('Score')
 
 plt.title('Elbow Curve')  
 
-mBkMeans4cl = MiniBatchKMeans(n_clusters=4,init='random',max_iter=20,
+numCl = 7
+mBkMeansCl = MiniBatchKMeans(n_clusters=numCl,init='random',max_iter=20,
                            batch_size=np.round(cIn[:,iR[imgInPoly_i],iC[imgInPoly_i]].transpose().shape[0]*s_size).astype(int),verbose=False,
                            compute_labels=True,random_state=42)
-clusterFit= mBkMeans4cl.fit(cIn[:,iR[imgInPoly_i],iC[imgInPoly_i]].transpose())
-clMems = mBkMeans4cl.labels_
+clusterFit= mBkMeansCl.fit(cIn[:,iR[imgInPoly_i],iC[imgInPoly_i]].transpose())
+clMems = mBkMeansCl.labels_
 
 
-imgTemp3 = imgTemp
+imgTemp3 = imgTemp.copy()
 imgTemp3[iR[imgInPoly_i],iC[imgInPoly_i]]=(clMems+1).transpose()
-imgTemp4 = imgTemp3.reshape(iR,iC,order='F')
+#imgTemp4 = imgTemp3.reshape(iR,iC,order='F')
 
 plt.figure()
 plt.imshow(imgTemp3)
